5b565adde190673e6c51bf2c5b51cc8a6d83220d
dspinoz <dspinoz@gmail.com>
2012-09-14 01:02:30 +0930

    update comments

--- BlinkingLED/BlinkingLED.ino
+++ BlinkingLED/BlinkingLED.ino
@@ -1,21 +1,16 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
+//BlinkingLED
+//Turns on an LED on for one second, then off for one second, repeatedly.
+//dspinoz@spinoz.com.au
  
 int pin = 8;
 
 void setup() {                
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
   pinMode(pin, OUTPUT);     
 }
 
 void loop() {
-  digitalWrite(pin, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(pin, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+  digitalWrite(pin, HIGH);
+  delay(1000);
+  digitalWrite(pin, LOW);
+  delay(1000);
 }
